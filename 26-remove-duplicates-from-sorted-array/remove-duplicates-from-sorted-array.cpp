edb24567267f9a512184d42a362dab2f107abbab
ayscoding <asuwan@uwo.ca>
2026-02-16 08:52:34 -0500

    Time: 0 ms (100.00%) | Memory: 22.6 MB (52.14%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int write = 1;
+
+        for (int read = 1; read<nums.size(); read++){
+            if (nums[read] != nums[write -1]){
+                nums[write] = nums[read];
+                write ++;
+            }
+        }
+
+        return write;
+    }
+};
